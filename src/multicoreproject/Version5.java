c907a3dcdb84bde8adb75e0c5d653f1fccfff00d
giovanniani <giovanni_ani@hotmail.com>
2021-12-07 10:42:22 -0600

    branch 5 started

---
+++ src/multicoreproject/Version5.java
@@ -0,0 +1,48 @@
+package multicoreproject;
+
+import multicoreproject.version2.BoundaryFinder;
+import multicoreproject.version2.Boundary;
+import multicoreproject.version4.GridBuilder;
+import multicoreproject.version4.Grid;
+
+public class Version5 extends VersionObject {
+	private int[][] _grid;
+	private int _total_pop;
+
+	public Version5(CensusData data, int grid_x, int grid_y) {
+		// Calculate the bounds
+		Boundary bounds = BoundaryFinder.findBoundaries(data);
+		// Build the grid
+		_grid = GridBuilder.buildGrid(data, grid_x, grid_y, bounds);
+
+		// Step 2: Update the grid so that each cell holds the total
+		// population for all cells that are farther south or farther
+		// west
+		_total_pop = 0;
+		for (int x = 0; x < grid_x; ++x) {
+			for (int y = 0; y < grid_y; ++y) {
+				// Also calculate the total population here
+				_total_pop += _grid[x][y];
+				int tmp0 = 0;
+				if (y > 0) {
+					tmp0 = _grid[x][y - 1];
+				}
+				int tmp1 = 0;
+				if (x > 0) {
+					tmp1 = _grid[x - 1][y];
+				}
+				int tmp2 = 0;
+				if (x > 0 && y > 0) {
+					tmp2 = _grid[x - 1][y - 1];
+				}
+				_grid[x][y] = _grid[x][y] + tmp0 + tmp1 - tmp2;
+			}
+		}
+	}
+
+	QueryResult query(int min_x, int min_y, int max_x, int max_y) {
+		// TODO: This query isn't quite right
+		int pop = _grid[max_x - 1][max_y - 1] - (_grid[min_x - 1][max_y - 1] + _grid[max_x - 1][min_y - 1] - _grid[min_x - 1][min_y - 1]);
+		return new QueryResult(pop, ((float)100.0) * ((float)pop) / _total_pop);
+	}
+}
